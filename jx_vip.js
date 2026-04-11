23dfe30dfb62d11a334a0074010cf196a4841fd6
taotaomimao <lx20240807vip@gmail.com>
2026-04-11 22:19:22 +0800

    Update jx_vip.js

--- jx_vip.js
+++ jx_vip.js
@@ -1,43 +1,21 @@
 /*
 [rewrite_local]
-^https:\/\/mp\.jxnewbook\.cn\/api\/additional-server\/v1\/pay\/member\/status url script-response-body https://raw.githubusercontent.com/taotaomimao/taotaomimao/main/jx_vip.js
+^https://mp.jxnewbook.cn/api/additional-server/v1/pay/member/status url script-response-body https://raw.githubusercontent.com/taotaomimao/taotaomimao/main/jx_vip.js
+^https://mp.jxnewbook.cn/api/newbook-user-center/v1/thirdpartyUser/getByOpenId url script-response-body https://raw.githubusercontent.com/taotaomimao/taotaomimao/main/jx_vip.js
 
 [mitm]
 hostname = mp.jxnewbook.cn
 */
 
-var body = $response.body;
-var headers = $response.headers;
+var url = $request.url;
+var obj = JSON.parse($response.body);
 
-try {
-    var obj = JSON.parse(body);
-
-    if (obj.data) {
+if (url.indexOf("member/status") !== -1) {
+    if (obj && obj.data) {
         var d = obj.data;
-
         d.status = 1;
         d.status_text = "已生效";
         d.effect_to = 4070880000000;
-        d.vip_time_type = 10;
-        d.vip_payment_type = true;
-
-        if (d.memberships && d.memberships.length > 0) {
-            d.memberships[0].effect_to = 4070880000000;
-            d.memberships[0].result = 1;
-            d.memberships[0].level = 10;
-            d.memberships[0].payment = 22;
-            d.memberships[0].cppid = 1;
-        }
-    }
-
-    body = JSON.stringify(obj);
-    console.log("✅ VIP修改成功");
-} catch (e) {
-    console.log("❌ 出错：" + e.message);
-}
-
-$done({ body, headers });
-
         d.vip_time_type = 10;
         d.vip_payment_type = true;
 
@@ -50,13 +28,15 @@ $done({ body, headers });
             d.memberships[0].cppid = 1;
         }
     }
-
-    body = JSON.stringify(obj);
-    console.log("✅ VIP修改成功");
-} catch (e) {
-    console.log("❌ 出错：" + e.message);
 }
 
-$done({ body, headers });
+if (url.indexOf("getByOpenId") !== -1) {
+    if (obj && obj.data) {
+        obj.data.groupId = 1;
+    }
+}
+
+$done({body: JSON.stringify(obj)});
+
 
 
