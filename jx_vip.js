03757bb590fb06d22ddadf5f240e74ef7cd060a1
taotaomimao <lx20240807vip@gmail.com>
2026-04-11 23:26:33 +0800

    Update jx_vip.js

--- jx_vip.js
+++ jx_vip.js
@@ -8,32 +8,45 @@ hostname = mp.jxnewbook.cn
 */
 
 var url = $request.url;
-var obj = JSON.parse($response.body);
 
-if (url.indexOf("member/status") !== -1) {
-    if (obj && obj.data) {
-        var d = obj.data;
-        d.status = 1;
-        d.status_text = "已生效";
-        d.effect_to = 4070880000000;
-        d.vip_time_type = 10;
-        d.vip_payment_type = true;
+// 增加安全检查：确保有响应体才进行解析
+if ($response.body) {
+    try {
+        var obj = JSON.parse($response.body);
 
-        if (d.memberships && d.memberships.length > 0) {
-            d.memberships[0].effect_to = 4070880000000;
-            d.memberships[0].effect_days = 30;
-            d.memberships[0].result = 1;
-            d.memberships[0].level = 10;
-            d.memberships[0].payment = 22;
-            d.memberships[0].cppid = 1;
+        // 处理会员状态接口
+        if (url.indexOf("member/status") !== -1) {
+            if (obj && obj.data) {
+                var d = obj.data;
+                d.status = 1;
+                d.status_text = "已生效";
+                d.effect_to = 4070880000000;
+                d.vip_time_type = 10;
+                d.vip_payment_type = true;
+
+                if (d.memberships && Array.isArray(d.memberships) && d.memberships.length > 0) {
+                    d.memberships[0].effect_to = 4070880000000;
+                    d.memberships[0].effect_days = 30;
+                    d.memberships[0].result = 1;
+                    d.memberships[0].level = 10;
+                    d.memberships[0].payment = 22;
+                    d.memberships[0].cppid = 1;
+                }
+            }
         }
-    }
-}
 
-if (url.indexOf("getByOpenId") !== -1) {
-    if (obj && obj.data) {
-        obj.data.groupId = 1;
-    }
-}
+        // 处理用户 ID 接口
+        if (url.indexOf("getByOpenId") !== -1) {
+            if (obj && obj.data) {
+                obj.data.groupId = 1;
+            }
+        }
 
-$done({body: JSON.stringify(obj)});
+        $done({ body: JSON.stringify(obj) });
+    } catch (e) {
+        console.log("脚本执行异常: " + e);
+        $done({}); // 发生解析错误时，返回原始数据，不阻塞请求
+    }
+} else {
+    $done({}); // 没有响应体，直接跳过
+}
