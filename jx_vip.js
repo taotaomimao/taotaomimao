e6a94f53d4358d989293128b1ecd57760cb928eb
taotaomimao <lx20240807vip@gmail.com>
2026-04-11 23:22:38 +0800

    Update jx_vip.js

--- jx_vip.js
+++ jx_vip.js
@@ -1,3 +1,4 @@
+/*
 [rewrite_local]
 ^https://mp.jxnewbook.cn/api/additional-server/v1/pay/member/status url script-response-body https://raw.githubusercontent.com/taotaomimao/taotaomimao/main/jx_vip.js
 ^https://mp.jxnewbook.cn/api/newbook-user-center/v1/thirdpartyUser/getByOpenId url script-response-body https://raw.githubusercontent.com/taotaomimao/taotaomimao/main/jx_vip.js
