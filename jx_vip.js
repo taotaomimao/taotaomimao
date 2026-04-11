4f5d6113e3a6f163ba7260dde1fc3888bd43f55d
taotaomimao <lx20240807vip@gmail.com>
2026-04-11 19:15:09 +0800

    Update jx_vip.js

--- jx_vip.js
+++ jx_vip.js
@@ -17,14 +17,16 @@ try {
 
         d.status = 1;
         d.status_text = "已生效";
-        d.need_remind = false;
-        d.vip_payment_type = true;
         d.effect_to = 4070880000000;
+        d.vip_time_type = 10;
+        d.vip_payment_type = true;
 
         if (d.memberships && d.memberships.length > 0) {
             d.memberships[0].effect_to = 4070880000000;
             d.memberships[0].result = 1;
             d.memberships[0].level = 10;
+            d.memberships[0].payment = 22;
+            d.memberships[0].cppid = 1;
         }
     }
 
@@ -35,6 +37,7 @@ try {
 }
 
 $done({ body, headers });
+
 
 
 
