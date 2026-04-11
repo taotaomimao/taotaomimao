9a6b31e6923c514a5b64e5e3a70360d14bae0918
taotaomimao <lx20240807vip@gmail.com>
2026-04-11 22:00:57 +0800

    Update jx_vip.js

--- jx_vip.js
+++ jx_vip.js
@@ -1,22 +1,42 @@
-var obj = JSON.parse($response.body);
+/*
+[rewrite_local]
+^https:\/\/mp\.jxnewbook\.cn\/api\/additional-server\/v1\/pay\/member\/status url script-response-body https://raw.githubusercontent.com/taotaomimao/taotaomimao/main/jx_vip.js
 
-if (obj && obj.data) {
-    var d = obj.data;
-    d.status = 1;
-    d.status_text = "已生效";
-    d.effect_to = 4070880000000;
-    d.vip_time_type = 10;
-    d.vip_payment_type = true;
+[mitm]
+hostname = mp.jxnewbook.cn
+*/
 
-    if (d.memberships && d.memberships.length > 0) {
-        d.memberships[0].effect_to = 4070880000000;
-        d.memberships[0].effect_days = 30;
-        d.memberships[0].result = 1;
-        d.memberships[0].level = 10;
-        d.memberships[0].payment = 22;
-        d.memberships[0].cppid = 1;
+var body = $response.body;
+var headers = $response.headers;
+
+try {
+    var obj = JSON.parse(body);
+
+    if (obj.data) {
+        var d = obj.data;
+
+        d.status = 1;
+        d.status_text = "已生效";
+        d.effect_to = 4070880000000;
+        d.vip_time_type = 10;
+        d.vip_payment_type = true;
+
+        if (d.memberships && d.memberships.length > 0) {
+            d.memberships[0].effect_to = 4070880000000;
+            d.memberships[0].effect_days = 30;
+            d.memberships[0].result = 1;
+            d.memberships[0].level = 10;
+            d.memberships[0].payment = 22;
+            d.memberships[0].cppid = 1;
+        }
     }
+
+    body = JSON.stringify(obj);
+    console.log("✅ VIP修改成功");
+} catch (e) {
+    console.log("❌ 出错：" + e.message);
 }
 
-$done({body: JSON.stringify(obj)});
+$done({ body, headers });
+
 
